e7cd79f1a37f452e7fe3996bee9f4133449fdc71
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-10-02 08:05:00 +0600

    todo-rec

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo></Todo>
     </div>
   );
+}
+
+const Todo=()=>{
+  const [todos,setTodo]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res=>res.json())
+    .then(data=>setTodo(data));
+  },[]);
+  return (
+    <div>
+    {
+      todos.map(todo=><h1 style={{color:'blue',borderBottom:'1px solid red',paddingBottom:'5px'}} key={todo.id}>{todo.id}. {todo.title}</h1>)
+    }
+    </div>
+    
+  )
 }
 
 export default App;
